e939beeae7bd9ad5b3479df6ba038fcfbded0920
Forrest Iandola <fiandola@gmail.com>
2014-09-18 22:42:51 -0400

    random class as a web service is working

--- backend/test_get_class.py
+++ backend/test_get_class.py
@@ -1,8 +1,14 @@
+from pprint import pprint
 import get_class
 
-def test_get_class():
+def test_get_class_simple():
     for i in xrange(0, 10):
         print get_class.getSampleClass()
 
+def test_get_class():
+    for i in xrange(0, 10):
+        pprint(get_class.getRandomClass_berkeley())
+
+#test_get_class_simple()
 test_get_class()
 
